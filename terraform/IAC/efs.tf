0f0d5b68af80043636327f094fbf16c3da70b8c7
sally nader <SALLY.NADER.AHMED@hotmail.com>
2022-07-01 11:36:34 +0200

    Edits efs mount block.

--- terraform/IAC/efs.tf
+++ terraform/IAC/efs.tf
@@ -13,7 +13,7 @@ resource "aws_efs_mount_target" "mount" {
   security_groups = [aws_security_group.bastion-host.id]
 }
 
-resource "null_resource" "null_vol_attach"  {
+resource "null_resource" "attach_efs_bastion"  {
   depends_on = [
     aws_efs_mount_target.mount,
   ]
@@ -22,6 +22,26 @@ resource "null_resource" "null_vol_attach"  {
     user     = "ec2-user"
     private_key =  tls_private_key.key.private_key_pem
     host     = aws_instance.bastion.public_ip
+  }
+  provisioner "remote-exec" {
+    inline = [
+      "sudo mkdir /project",
+      "sudo mount -t nfs4 -o nfsvers=4.1,rsize=1048576,wsize=1048576,hard,timeo=600,retrans=2,noresvport ${aws_efs_file_system.efs.dns_name}:/ /project"
+     
+    ]
+  }
+}
+
+
+resource "null_resource" "attach_efs_kubernetes"  {
+  depends_on = [
+    aws_efs_mount_target.mount,
+  ]
+  connection {
+    type     = "ssh"
+    user     = "ec2-user"
+    private_key =  tls_private_key.key.private_key_pem
+    host     = aws_instance.kubernetes.private_ip
   }
   provisioner "remote-exec" {
     inline = [
